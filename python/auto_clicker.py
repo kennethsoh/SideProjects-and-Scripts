d1b441567138d631cf77577f6473a34cd255a59b
Kenneth Soh <34814795+kennethsoh@users.noreply.github.com>
2020-11-07 11:52:38 +0800

    Create auto_clicker.py

---
+++ python/auto_clicker.py
@@ -0,0 +1,9 @@
+// This script uses pyautogui to repeatedly click on a x & y coordinate on your screen.
+
+import pyautogui
+i = 1
+x = input("X coordinate: ")
+y = input("Y coordinate: ")
+while i != 0:
+    # x and y coordinates for pointer location
+    pyautogui.click(x, y)
